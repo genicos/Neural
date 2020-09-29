d6055b2bc80b96ba83f8690bf0997cb1dc43845b
Nicolas Ayala <nmayala@ucsc.edu>
2020-09-29 12:32:07 -0700

    Started network container

--- c_code/network/.h
+++ c_code/network/.h
@@ -5,8 +5,31 @@
 #include <inttypes.h>
 #include "../tensor/.h"
 
+typedef PARENTS_LENGTH uint8_t
+typedef NODES_LENGTH uint8_t
+
 typedef struct node{
   tensor *t;
-  uint8_t causes_length;
-  tensor **causes;
+  PARENTS_LENGTH parents_length;
+  tensor **parents;
 }
+
+typedef struct network{
+  NODES_LENGTH nodes_length;
+  node* nodes;
+}
+
+bool save_network(network *w, char *file_name);
+
+network *read_network(char *file_name);
+
+
+
+void node_delete(node *n);
+
+node *node_create(tensor *t, PARENTS_LENGTH parents_length, tensor **parents);
+
+void network_delete(network *w);
+
+network *network_create(NODES_LENGTH nodes_length,node *nodes);
+
